29cb02401e41c27954069c803466e6a359fced5b
FaizNaeem <faiznaeem192022@gmail.com>
2023-10-02 16:38:33 +0500

    continue

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -11,12 +11,23 @@ import React, { useEffect, useState } from 'react';
 export default function Faiz() {
   const [ todo , setTodo]= useState([])
   const [input ,setInput]=useState('')
+  const
   const Submit = () => {
 setTodo([...todo ,input])
 setInput("")
   }
-  const Edit=()=>{
+  const Edit=(e)=>{
+// const hello = todo.filter((e,i)=>{
+// if(n==i){
 
+// }
+// })
+const store = todo.find((ele) => e == ele);
+console.log(store);
+// setBtn(false);
+setInput(store);
+// Submit()
+// setSelect(e);
   }
   const Delete=(i)=>{
     const save = todo.filter((e,n) => i !==n);
@@ -52,7 +63,7 @@ setInput("")
               <button className='btn btn-sm hi btn-danger' onClick={()=>Delete(i)}>
                 <i className="fa-solid fa-trash"></i>
               </button>
-              <button className='btn btn-success btn-sm '  onClick={()=>Edit()}>
+              <button className='btn btn-success btn-sm '  onClick={()=>Edit(e)}>
                 <i className="fa-solid fa-pen-to-square"></i>
               </button>
             </h6>
@@ -63,3 +74,70 @@ setInput("")
     </>
   );
 }
+// import React, { useEffect, useState } from 'react';
+
+// export default function Faiz() {
+//   const [todo, setTodo] = useState([]);
+//   const [input, setInput] = useState('');
+//   const [editIndex, setEditIndex] = useState(null); // New state to track the index being edited
+
+//   const handleSubmit = () => {
+//     if (editIndex !== null) {
+//       // If an index is set, update the item at that index
+//       const updatedTodo = [...todo];
+//       updatedTodo[editIndex] = input;
+//       setTodo(updatedTodo);
+//       setInput('');
+//       setEditIndex(null);
+//     } else {
+//       // If no index is set, add a new item
+//       setTodo([...todo, input]);
+//       setInput('');
+//     }
+//   };
+
+//   const handleEdit = (i) => {
+//     setInput(todo[i]); // Set the input value to the item being edited
+//     setEditIndex(i); // Set the edit index to the item's index
+//   };
+
+//   const handleDelete = (i) => {
+//     const updatedTodo = todo.filter((_, index) => index !== i);
+//     setTodo(updatedTodo);
+//   };
+
+//   return (
+//     <>
+//       <h1 className='text-center'>Todo List</h1>
+//       <div className='center'>
+//         <div className="input-group mb-3">
+//           <input
+//             type="text"
+//             className="form-control"
+//             placeholder="Add Todo"
+//             value={input}
+//             required
+//             onChange={(e) => setInput(e.target.value)}
+//           />
+//           <button className='btn btn-success' type='submit' onClick={handleSubmit}>
+//             {editIndex !== null ? 'Edit' : 'Add'}
+//           </button>
+//         </div>
+//         {todo.map((e, i) => (
+//           <ul className='row' key={i}>
+//             <h6>
+//               {e}
+//               <button className='btn btn-sm btn-danger' onClick={() => handleDelete(i)}>
+//                 <i className="fa-solid fa-trash"></i>
+//               </button>
+//               <button className='btn btn-success btn-sm' onClick={() => handleEdit(i)}>
+//                 <i className="fa-solid fa-pen-to-square"></i>
+//               </button>
+//             </h6>
+//             <hr />
+//           </ul>
+//         ))}
+//       </div>
+//     </>
+//   );
+// }
